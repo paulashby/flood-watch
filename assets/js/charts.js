75de99b2f05b3cf45aed7865f5ddb2d697a84506
Sam B <reposme@outlook.com>
2023-01-09 08:20:34 +0000

    Added new properties to style the charts as well as an ajax call to insert the current number of flood events

--- assets/js/charts.js
+++ assets/js/charts.js
@@ -1,93 +1,210 @@
-// // This is the script that 'gets' the EA API data for the charts
+// This is the script that 'gets' the EA API data for the charts
 
-// var queryURL = "https://environment.data.gov.uk/flood-monitoring/id/floods";
+var queryURL = "https://environment.data.gov.uk/flood-monitoring/id/floods";
 
-//   //get JSON from EA Flood Monitoring for 'isTidal'
-//   $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   }).then(function (response) {
-//     // console.log(response);
-//     console.log(response.items[0].isTidal);
-//     // //logs the temperature of the first array item
-//     // console.log(response.list[0].main.temp);
-
-
-//   });
+// //get JSON from EA Flood Monitoring for 'isTidal'
+// $.ajax({
+//   url: queryURL,
+//   method: "GET"
+// }).then(function (response) {
+//   // console.log(response);
+//   console.log(response.items[0].isTidal);
+//   // //logs the temperature of the first array item
+//   // console.log(response.list[0].main.temp);
 
 
-//     //get JSON from EA Flood Monitoring for 'isTidal'
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//       }).then(function (response) {
-//         // console.log(response);
-//         console.log(response.items[0].isTidal);
-//         // //logs the temperature of the first array item
-//         // console.log(response.list[0].main.temp);
-//     console.log(response.items);
+// });
 
-
-// // jQuery loop over JSON result after AJAX Success (https://www.etutorialspoint.com/index.php/46-jquery-loop-over-json-result-after-ajax-success)
+// var index = []
 
 
 
-// // Then count all 'isTidal' key: values:
+// return items;
 
-//         // if 'false' count
+//get JSON from EA Flood Monitoring for 'isTidal'
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function (response) {
+  // console.log(response);
+  console.log(response.items[0].isTidal);
+  // //logs the temperature of the first array item
+  console.log(response.items);
+  // console.log(response.items[i].description);
 
-//         // if 'true' count
+  var numberOfFloods = response.items.length
+
+  console.log(numberOfFloods);
+
+  // var keys = Object.keys(response.items[0]);
+  // console.log(keys);
+
+  //Append 'Number of Floods' to #numberOfFloods
+
+  var numberOfFloodsEl = document.getElementById('numberOfFloods');
+
+  numberOfFloodsEl.innerHTML += ('There are ' + numberOfFloods + ' flood warnings in place');
+
+  // sets the colour of #numberOfFloods
+  numberOfFloodsEl.setAttribute('style', 'color: rgb(16, 253, 28);')
 
 
-//       });
+  // jQuery loop over JSON result after AJAX Success (https://www.etutorialspoint.com/index.php/46-jquery-loop-over-json-result-after-ajax-success)
 
-// This is the script that renders each chart
 
-// Chart #1
+
+  // Then count all 'isTidal' key: values:
+
+  // if 'false' count
+
+  // if 'true' count
+
+
+
+
+  // This is the script that renders each chart
+
+  // Chart #1 - Number of Floods
   var options = {
+    series: [numberOfFloods],
     chart: {
-      type: 'bar'
+      type: 'donut',
     },
-    colors: ['rgb(16, 253, 28)'],
-    series: [{
-      name: 'isTidal',
-      data: [30,40]
-    }],
-    xaxis: {
-      categories: ['True','False']
-    }
-  }
-  
+    colors: ['rgba(16, 253, 28, 0.25)'],
+    stroke: {
+      show: true,
+      curve: 'smooth',
+      lineCap: 'butt',
+      colors: ['rgb(16, 253, 28)'],
+      width: 2,
+      dashArray: 0,
+    },
+    dataLabels: {
+      enabled: true,
+      enabledOnSeries: undefined,
+      formatter: function (val, opts) {
+        return val
+      }
+    },
+    legend: {
+      show: false,
+    },
+
+    responsive: [{
+      breakpoint: 480,
+      options: {
+        chart: {
+          width: 200
+        },
+        legend: {
+          position: 'bottom'
+        }
+      }
+    }]
+  };
+
   var chart = new ApexCharts(document.querySelector("#chartOne"), options);
-  
+
   chart.render();
 
   // Chart #2
-
   var options = {
     chart: {
-      type: 'bar'
+      type: 'bar',
+      toolbar: {
+        show: false,
+      },
+      height: '25%'
     },
-    colors: ['rgb(16, 253, 28)'],
+
+    grid: {
+      show: true,
+      borderColor: 'rgba(16, 253, 28, 0.25)',
+      strokeDashArray: 0,
+      position: 'back',
+      xaxis: {
+        lines: {
+          show: true
+        }
+      },
+      yaxis: {
+        lines: {
+          show: true
+        }
+      },
+      row: {
+        colors: undefined,
+        opacity: 0.5
+      },
+      column: {
+        colors: undefined,
+        opacity: 0.5
+      },
+      padding: {
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0
+      },
+    },
+    colors: ['rgba(16, 253, 28, 0.25)'],
+    stroke: {
+      show: true,
+      curve: 'smooth',
+      lineCap: 'butt',
+      colors: ['rgb(16, 253, 28)'],
+      width: 2,
+      dashArray: 0,
+    },
     series: [{
-      name: 'sales',
-      data: [30,40]
+      name: 'isTidal',
+      data: [numberOfFloods]
     }],
     xaxis: {
-      categories: ['True','False']
+      categories: ['Number'],
+      labels: {
+        show: false,
+      }
+    },
+    yaxis: {
+      categories: ['Number'],
+      labels: {
+        show: true,
+        style: {
+          colors: ['rgba(16, 253, 28, 0.5)'],
+          fontSize: '12px',
+          fontFamily: 'Helvetica, Arial, sans-serif',
+          fontWeight: 400,
+          cssClass: 'apexcharts-xaxis-label',
+        },
+        offsetX: -10,
+        offsetY: 0,
+      },
+      axisBorder: {
+        show: false,
+        color: '#000000',
+        height: 0,
+        width: 0,
+        offsetX: 0,
+        offsetY: 0
+      },
     }
+
+
   }
-  
+
   var chart = new ApexCharts(document.querySelector("#chartTwo"), options);
-  
+
   chart.render();
 
   // Chart #3
-
   var options = {
     chart: {
       height: 280,
-      type: "area"
+      type: "area",
+      toolbar: {
+        show: false,
+      }
     },
     dataLabels: {
       enabled: false
@@ -100,7 +217,15 @@
     ],
     fill: {
       type: "solid",
+      colors: ['rgba(16, 253, 28, 0.25)'],
+    },
+    stroke: {
+      show: true,
+      curve: 'smooth',
+      lineCap: 'butt',
       colors: ['rgb(16, 253, 28)'],
+      width: 2,
+      dashArray: 0,
     },
     xaxis: {
       categories: [
@@ -112,9 +237,90 @@
         "06 Jan",
         "07 Jan"
       ]
+    },
+    grid: {
+      show: true,
+      borderColor: 'rgba(16, 253, 28, 0.25)',
+      strokeDashArray: 0,
+      position: 'back',
+      xaxis: {
+        lines: {
+          show: true
+        }
+      },
+      yaxis: {
+        lines: {
+          show: true
+        }
+      },
+      row: {
+        colors: undefined,
+        opacity: 0.5
+      },
+      column: {
+        colors: undefined,
+        opacity: 0.5
+      },
+      padding: {
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0
+      },
+      labels: {
+        show: false,
+      }
+    },
+    yaxis: {
+      categories: ['Number'],
+      labels: {
+        show: true,
+        style: {
+          colors: ['rgba(16, 253, 28, 0.5)'],
+          fontSize: '12px',
+          fontFamily: 'Helvetica, Arial, sans-serif',
+          fontWeight: 400,
+          cssClass: 'apexcharts-xaxis-label',
+        },
+        offsetX: -10,
+        offsetY: 0,
+      },
+      axisBorder: {
+        show: false,
+        color: '#000000',
+        height: 0,
+        width: 0,
+        offsetX: 0,
+        offsetY: 0
+      },
     }
+
   };
-  
+
   var chart = new ApexCharts(document.querySelector("#chartThree"), options);
-  
+
+
   chart.render();
+
+  // // Chart #4
+
+  // var options = {
+  //   chart: {
+  //     type: 'bar'
+  //   },
+  //   colors: ['rgb(16, 253, 28)'],
+  //   series: [{
+  //     name: 'sales',
+  //     data: [30,40]
+  //   }],
+  //   xaxis: {
+  //     categories: ['True','False']
+  //   }
+  // }
+
+  // var chart = new ApexCharts(document.querySelector("#chartTwo"), options);
+
+  // chart.render();
+
+
+});
