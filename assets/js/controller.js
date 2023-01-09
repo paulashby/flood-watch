b23b0e89fe7e53c001854ce91446fac4e052d715
Paul Ashby <paul@primitive.co>
2023-01-09 18:48:08 +0000

    Submit location on enter, reset view when input field cleared

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -4,14 +4,31 @@ const locationInput = locationElmts.find("#location-input");
 const API_KEY = "a5ecca38271f404f9ae5f7f8c2d005d6";
 
 locationBtn.on("click", onLocation);
+locationInput.on("keypress", onLocationKeypress);
+locationInput.on("keyup", onLocationClear)
 
-function onLocation(e) {
+function onLocationKeypress(e) {    
+    const keycode = (e.keyCode ? e.keyCode : e.which);
+    if (keycode == '13') {
+       onLocation();
+    }
+    // Prevent propogation to other possible handlers
+    e.stopPropagation();
+}
+
+function onLocationClear() {
+    if (!this.value) {
+        $(window).trigger("home");
+    }
+}
+
+function onLocation() {
     const location = locationInput.val().trim();
 
     if (location.length) {
         const geoQueryURL = encodeURI(`https://api.opencagedata.com/geocode/v1/json?q=${location}&no_annotations=1&key=${API_KEY}`);
 
-        $ajax({
+        $.ajax({
             url: geoQueryURL,
             method: "GET"
         })
@@ -24,35 +41,30 @@ function onLocation(e) {
                     const lat = response.results[0].geometry.lat;
                     const lng = response.results[0].geometry.lng;
                     const warningsURL = `https://environment.data.gov.uk/flood-monitoring/id/floods?lat=${lat}&long=${lng}&dist=40`;
+                    const items = [];
+
+                    $.ajax({
+                        url: warningsURL,
+                        method: "GET"
+                    })
+                        .then(function (response) {
+
+                            if (!response.items.length) {
+                                // No data
+                                console.log("No data available - ideally switch to a fallback API");
+                            } else {
+                                itemData = response.items;
+                                // Trigger event for interested parties
+                                $(window).trigger("location", [{
+                                    location: location,
+                                    items: itemData
+                                }]);
+                            }
+                        });
                 }
             });
 
-        let warningsURL = "https://environment.data.gov.uk/flood-monitoring/id/floods?lat=51.5072&long=0.1276&dist=40";
-        let items = [];
-
-        $.ajax({
-            url: warningsURL,
-            method: "GET"
-        })
-            .then(function (response) {
-
-                if (!response.items.length) {
-                    // No data
-                    console.log("No data available - ideally switch to a fallback API");
-                } else {
-                    itemData = response.items;
-                    const locationEvent = new CustomEvent('changeLocation', {
-                        detail: {
-                            location: location,
-                            items: itemData
-                        }
-                    });
-                    this.dispatchEvent(locationEvent);
-                }
-            });
     } else {
         console.log("Please enter the name of a town, city or county");
     }
-
-
 }
